a6de4662bfc89ee20786244566d6c9b6953f16f6
wata0723sw <wata0723sw@icloud.com>
2025-08-02 12:24:28 +0900

    Create PbjIrisShadowFixMod.java

---
+++ src/main/java/your/name/pbjshadowfix/PbjIrisShadowFixMod.java
@@ -0,0 +1,13 @@
+package your.name.pbjshadowfix;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Minimal entrypoint for Forge to recognize the mod.
+ */
+@Mod("pbj_iris_shadowfix")
+public class PbjIrisShadowFixMod {
+    public PbjIrisShadowFixMod() {
+        // Nothing to do. Mixins are declared in mods.toml and will be applied on load.
+    }
+}
